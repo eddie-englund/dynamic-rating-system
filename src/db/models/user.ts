7f33bd0ad27838f4f60a8f0d6a492dc1eecb0c85
eddieenglund <eddie.englund@protonmail.com>
2022-09-26 21:55:13 +0200

    roles

--- src/db/models/user.ts
+++ src/db/models/user.ts
@@ -1,7 +1,28 @@
 import { ObjectId } from "mongodb";
 
+export enum Permissions {
+  createPost = "create:post",
+  editOwnPost = "edit-own:post",
+  editPosts = "edit:post",
+  deletePosts = "delete:post",
+  deleteOwnPost = "delete-own:post"
+}
+
+export const BasePermissions = [
+  Permissions.createPost,
+  Permissions.editOwnPost,
+  Permissions.deleteOwnPost
+]
+
+export const AdminPermissions = [
+  ...BasePermissions,
+  Permissions.deletePosts,
+  Permissions.editPosts
+]
+
 export type User = {
   _id?: ObjectId,
   username: string,
   password: string,
+  permissions: String[]
 }
